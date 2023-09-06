3a6720360909d271e80bfebd88e16dcad20c5558
Sofía Hernández <sofiachp95@gmail.com>
2023-09-06 18:47:22 -0300

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,33 @@
+const url = 'https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+	}
+};
+
+function getJSONData(url, options){
+    let result = {};
+    showSpinner(); 
+    return fetch(url, options) 
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+          hideSpinner(); 
+          return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
+        hideSpinner(); 
+        return result;
+    });
+};
