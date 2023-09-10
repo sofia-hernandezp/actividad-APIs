f17a5eb3c2fae771df83fcd2e5a275bebb0d5947
Sofía Hernández <sofiachp95@gmail.com>
2023-09-10 12:43:55 -0300

    Final changes

--- js/init.js
+++ js/init.js
@@ -1,33 +1,68 @@
-const url = 'https://food-nutrition-information.p.rapidapi.com/food/1497465';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b17c5e7b79msh415b236d890006fp11c205jsnc78fc3d7f0ee',
-		'X-RapidAPI-Host': 'food-nutrition-information.p.rapidapi.com'
-	}
+const URL = "https://api.nationalize.io?name=";
+
+  function getJSONData(url){
+    let result = {};
+    return fetch(url) 
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+          return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;     
+        return result;
+    });
 };
 
-
-function getJSONData(url, options){
-  let result = {};
-  showSpinner(); 
-  return fetch(url, options) 
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    }else{
-      throw Error(response.statusText);
-    }
-  })
-  .then(function(response) {
-        result.status = 'ok';
-        result.data = response;
-        console.log(result.data); 
-        return result;
-  })
-  .catch(function(error) {
-      result.status = 'error';
-      result.data = error; 
-      return result;
-  });
+function generateCountryElements(countryData) {
+  let elements = '';
+  for (let i = 0; i < countryData.length; i++) {
+      elements += `
+          <div class= "card_image">
+              <img src="https://flagsapi.com/${countryData[i].country_id}/flat/64.png" alt="bandera ${countryData[i].country_id}">
+          </div>
+          <div class= "card_content">
+              <p class="card_text"> ${countryData[i].country_id} - ${countryData[i].probability} % </p>
+              <hr>
+          </div>
+    `;
+  }
+  return elements;
 }
+
+document.addEventListener("DOMContentLoaded", ()=>{
+ 
+  const input = document.getElementById("searchInput");
+  const btn = document.getElementById("searchButton"); 
+  
+  btn.addEventListener("click", () => {
+  
+    document.getElementById("container").innerHTML = ""
+
+      getJSONData(URL + input.value).then((response) => {
+        let nombre = response.data.name;
+
+        console.log(nombre);
+
+        document.getElementById("container").innerHTML += `
+        <li class="cards_item">
+            <div class="card">
+              <h2 class= "card_title">${nombre.toUpperCase()}</h2>
+              ${generateCountryElements(response.data.country)}
+            </div>
+        </li>
+        `
+
+      });
+  });
+});
+
+
